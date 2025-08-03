3c90a3b41e2572f5ec4a4112c4c42aa19f058cf2
Kris Amoateng <camoateng002@st.ug.edu.gh>
2025-08-03 13:24:38 -0700

    Initialize Finance Management System project structure with core files and configurations

---
+++ FinanceManagementSystem/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // i. Create an instance of FinanceApp and call Run()
+        var app = new FinanceApp();
+        app.Run();
+    }
+}
